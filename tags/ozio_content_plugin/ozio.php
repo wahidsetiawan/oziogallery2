377418d239e872676e3221c06683eaa0ae3dedff
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2009-07-25 13:42:46 +0000

    Test dimensione automatica iframe per inserimento galleria nei contenuti

--- tags/ozio_content_plugin/ozio.php
+++ tags/ozio_content_plugin/ozio.php
@@ -88,8 +88,78 @@ function plgcontentloadozio( $galleriaozio, $larghezza, $altezza, $scroll )
 		
 				@$gall 	= JURI::root(). $codice->link .'&Itemid='. $galleriaozio;
 
-			if (@$codice->published != 0 && @$codice->access != 1 && @$codice->access != 2) :	
-				$contents = '<iframe src ="'.$gall.'&amp;tmpl=component" width="'.$larghezza.'" height="'.$altezza.'" scrolling="'.$scroll.'" frameborder="0">';
+			if (@$codice->published != 0 && @$codice->access != 1 && @$codice->access != 2) :
+				$contents = '';
+				$document->addCustomTag('
+				<script type="text/javascript">
+				var iframeids=["main_'.$cp->id.'"]
+
+				var iframehide="yes"
+				var getFFVersion=navigator.userAgent.substring(navigator.userAgent.indexOf("Firefox")).split("/")[1]
+				var FFextraHeight=parseFloat(getFFVersion)>=0.1? 16 : 0 //extra height in px to add to iframe in FireFox 1.0+ browsers
+				function resizeCaller() 
+				{
+				var dyniframe=new Array()
+				for (i=0; i<iframeids.length; i++)
+					{
+						if (document.getElementById)
+						resizeIframe(iframeids[i])
+						//reveal iframe for lower end browsers? (see var above):
+						if ((document.all || document.getElementById) && iframehide=="no")
+						{
+							var tempobj=document.all? document.all[iframeids[i]] : document.getElementById(iframeids[i])
+							tempobj.style.display="block"
+						}
+					}
+				}
+
+				function resizeIframe(frameid)
+				{
+				var currentfr=document.getElementById(frameid)
+				if (currentfr && !window.opera)
+					{
+						currentfr.style.display="block"
+						if (currentfr.contentDocument && currentfr.contentDocument.body.offsetHeight) //ns6 syntax
+						currentfr.height = currentfr.contentDocument.body.offsetHeight+FFextraHeight;
+						else if (currentfr.Document && currentfr.Document.body.scrollHeight) //ie5+ syntax
+						currentfr.height = currentfr.Document.body.scrollHeight;
+						if (currentfr.addEventListener)
+						currentfr.addEventListener("load", readjustIframe, false)
+						else if (currentfr.attachEvent)
+						{
+							currentfr.detachEvent("onload", readjustIframe) // Bug fix line
+							currentfr.attachEvent("onload", readjustIframe)
+						}
+					}
+				}
+
+				function readjustIframe(loadevt) 
+				{
+					var crossevt=(window.event)? event : loadevt
+					var iframeroot=(crossevt.currentTarget)? crossevt.currentTarget : crossevt.srcElement
+					if (iframeroot)
+					resizeIframe(iframeroot.id);
+				}
+
+				function loadintoIframe(iframeid, url)
+				{
+					if (document.getElementById)
+					document.getElementById(iframeid).src=url
+				}
+
+				if (window.addEventListener)
+				window.addEventListener("load", resizeCaller, false)
+				else if (window.attachEvent)
+				window.attachEvent("onload", resizeCaller)
+				else
+				window.onload=resizeCaller
+
+				</script>
+
+				');	
+                $contents ='<div class="clr"></div>';				
+				$contents .= '<iframe id="main_'.$cp->id.'" name="main_'.$cp->id.'" src ="'.$gall.'&amp;tmpl=component"  scrolling="'.$scroll.'" frameborder="0" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0" style="overflow:visible; width:100% ; display:none" >';			
+//				$contents = '<iframe src ="'.$gall.'&amp;tmpl=component" width="'.$larghezza.'" height="'.$altezza.'" scrolling="'.$scroll.'" frameborder="0">';
 				$contents .= '<p>Your browser does not support iframes.</p>';
 				$contents .= '</iframe>';
 
